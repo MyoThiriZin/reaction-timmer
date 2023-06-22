c00302eae5b27f5003573f7bd02c3368a92cd9a3
MyoThiriZin <myothirizin99@gmail.com>
2023-06-22 11:49:58 +0630

    upload project

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <h1>How fast can u catch me?</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <div v-if="isPlaying">
+    <Block :delay="delay" @endGame="end"/>
+  </div>
+  <div v-if="finish">
+    <Result :score="score"/>
+  </div>
+</template>
+
+<script>
+import Block from "./components/Block.vue"
+import Result from "./components/Result.vue"
+export default {
+  name: 'App',
+  components: {
+    Block,
+    Result,
+  },
+  data(){
+    return {
+      isPlaying: false,
+      delay:null,
+      score:0,
+      finish: false,
+    }
+  },
+  methods:{
+      start(){
+        this.isPlaying=true;
+        this.delay=2000+Math.random()*5000;
+      },
+      end(score){
+        this.score=score;
+        this.isPlaying=false;
+        this.finish=true;
+      }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
